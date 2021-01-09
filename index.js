1c5973a1b7af759bfb3af322c8798c675624bc81
David Shonn <davidlshonn@icloud.com>
2021-01-09 17:40:16 +0000

    Initial commit - got path, fs and process working to create a file from scratch for readme

---
+++ index.js
@@ -0,0 +1,37 @@
+const generateMarkdown = require('./utils/generateMarkdown');
+const enquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+
+// array of questions for user
+const questions = [
+{
+    type:"input",
+    name:"title",
+    message:"What is the title of your application?"
+},
+{
+    type:"input",
+    name:"description",
+    message:"What is the description of your application?"
+}
+];
+
+// function to write README file
+function writeToFile(fileName, data) {
+    return fs.writeFileSync(fileName, data);
+}
+
+// function to initialize program
+function init() {
+    enquirer.prompt(questions).then((answers) =>{
+        console.log (answers);
+        console.log (process.cwd());
+        var newFileLocation = path.join(process.cwd(), "newReadMe.md");
+        var newReadmeInfo = generateMarkdown(answers);
+        writeToFile(newFileLocation, newReadmeInfo);
+    })
+}
+
+// function call to initialize program
+init();
---
+++ newReadMe.md
@@ -0,0 +1,5 @@
+# woien
+
+  ## description
+  
+  erveqf
